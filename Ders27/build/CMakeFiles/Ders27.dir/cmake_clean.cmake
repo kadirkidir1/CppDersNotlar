6c8029921af0dfda5334f87ea05f6fbf7de55c8d
ABDULKADIR <abdulkadir.kidir@asisct.com>
2022-12-16 17:18:17 +0300

    ders27 eklendi

---
+++ Ders27/build/CMakeFiles/Ders27.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ders27.dir/main.cpp.o"
+  "Ders27"
+  "Ders27.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ders27.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
