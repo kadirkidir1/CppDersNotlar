9f9011fb80a66754abafd66cbc1264e953c6c4fe
kadirkidir1 <56589680+kadirkidir1@users.noreply.github.com>
2024-06-21 02:14:27 +0300

    Add files via upload
    Ders 28 tamamlandi.

--- Ders28/main.cpp
+++ Ders28/main.cpp
@@ -515,6 +515,8 @@ void main_10() {
 //                    baslamadan onceki durumunu koruyacak. Yani ya isleme devam edecek, edemiyorsa da
 //                    exception'dan onceki adima geri donecek. Basic guarantee'de bu yok iste.
 
+// No Throw Guarantee
+
 #include <memory>
 #include <iostream>
 #include <vector>
@@ -525,7 +527,8 @@ using namespace std;
 class Message{
 
 public:
-    std::string to_string ()const {
+    std::string to_string ()const
+    {
         // Throw edebilir ya da etmeyebilir
         return "asdasd";
     }
@@ -543,41 +546,263 @@ void log(const std::vector<Message>& mvec) {
     log_file << result;
 }
 
+// Simdi yukaridaki koda bakarsak, vector'den mesajlar alinip result adli string'e
+// to_string metodundan donduktan sonra isleniyor. Ama herhangi bir anda, ornegin 5. nesnede
+// to_string metodu throw ederse, for'dan cikacak. Result degeri de bos olacak kaynak geri
+// verilerecek, ve bu sebepten oturu log_file dosyasina herhangi bir sey yazilmayacak. Iste
+// bu strong garanti. Hem memory leak&resource leak yok, hem de dosya, baslangictaki ayni
+// state'ine dondu. Strong garanti, ya hep ya hic yasasini benimser. Ama bu sekilde degil de
+// asagidaki gibi olsaydi,
+
+    void log_2(const std::vector<Message>& mvec) {
+    std::ofstream log_file;
+    log_file.open("log.txt", std::ios_base::app);
+    
+    for (const auto& m : mvec)
+        log_file << m.to_string();
+}
+
+// log_2 metodunda, herhangi bir throw esnasinda log.txt yine kapanacakti, herhangi bir leak
+// meydana gelmeyecekti ama!! ornegin 5. nesneye kadar yazilanlar, dosyaya kaydedilmis olacakti.
+// yani baslangictaki durumuna donmeyecekti. Iste bu basic guarantee.
+
+// En son garanti turu ise No Throw Guarantee.
+
+// Destructor'lar no throw garanti vermelidir.
+// swap fonksiyonları no throw garanti eder.
+// move members(ozellikler move constructor)
+// operator delete fonksiyonu gibi kaynak iade eden fonksiyonlar.
+// cleanup fonksiyonları
 
 
+// Asagida modern c++ oncesi anlatilacaktir. Cok dikkate alinmamali, dilden kaldirildi.
+
+void func_t(); // herhangi bir exception throw edebilir.
+// void func_t1()throw(std::bad_alloc, std::runtime_error); // hata verdi zaten. C++17 bunu desteklemez diye.
+                                                         // Ama kisaca olay su, yalnizca parantez icindeki
+                                                         // ex'leri throw edebiliriz.
+
+void func_t2()throw();// Eger ici bos ise, bu da exeption throw etmeme garantisidir.
+
+// NoExcept --> dile eklendikten
+
+///////////////////////////////////////////////////////////////////////////////////
+// Modern C++'ta eger NoExcept operator kullaniliyorsa, (dikkat edilirse NoExcept specifier
+// degil, NoExceptOperator), bunun anlami herhangi bir exception throw etmeyecek demektir.
+
+ // NoExcept, bildirimde de tanimda da yer almasi gerekiyor!!!
+// Yani header'da NoExcept bildirip, Cpp içerisinde bildirmemek olmaz!!!
+
+//// kdr.h
+//void knur()noexcept;
+//// kdr.cpp
+//void knur()noexcept() {
+
+//}
+// Bu sekilde olmasi gerekiyor. Direkt olarak, syntax hatasi olur.
+// Metot'lardan birisi noexcept ama digeri degil --> Bu sekilde overload edemeyiz.
+
+void funcnur()noexcept;
+void funcnur()noexcept(true);
+// Yukaridaki iki metot arasinda hicbir fark yok.
+
+void funckdr()noexcept(false);
+void funckdr();
+// Yukaridaki iki metot arasinda da hicbir fark yok.
+//
+
+// Bir fonksiyona noexcept garantisi verdigim halde eger throw ediyorsa,
+// bu compile time'da kontrol edilmedigi icin, set_terminate metot'una gidecektir.
+
+void func_072()noexcept{
+
+}
+
+void main_11() {
+    try {
+        func_072();
+    } catch (const std::exception& ex) {
+        std::cout << "buraya hic girmeyecek, cunku func_072 noexcept garanti verdi";
+        // set_terminate metotuna gidecek!!!
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+// special member function'lar noexcept kabul ediliyor. Bu dilin kurali!!!
+// destructor kesinlikle noexcept kabul ediliyor ama digerlerinde anladigim
+// kadariyla bazen degisebiliyor.
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+// noexcept olup olmadigini compile time'da anlayabildigimiz durumlar var. Bunu ileride
+// detayli gorecegimiz type_traits sagliyor!!
+
+#include <type_traits>
+
+class MyClass_000{
+
+};
+
+void main_12() {
+    const auto b = is_nothrow_default_constructible_v<MyClass_000>;
+    std::cout << "b'nin durumu: " << b << "\n"; // bu sekilde yazdirilirsa, 1 gorulur.
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+// Noexcept hayati onem tasiyan bir sey. Cunku STL'lere geldigimizde, derleyicinin
+// kod secerek, optimizasyon yapmasini sagliyor.
+/// //////////////////////////////////////////////////////////////////////////////////////
+
+#include <vector>
+
+class Sentence {
+public:
+    Sentence() = default;
+    Sentence(const char* ps) : m_s{ ps } {}
+    Sentence(const Sentence& other) : m_s{ other.m_s }
+    {
+        std::cout << "copy ctor\n";
+    }
+    Sentence(Sentence&& other)
+//    Sentence(Sentence&& other)noexcept
+        : m_s{ std::move(other.m_s) }
+    {
+        std::cout << "move ctor\n";
+    }
+
+private:
+    std::string m_s;
+};
+
+void main_13()
+{
+    std::vector<Sentence> svec{
+                               "Consuetudinis magna vis est",
+                               "Homines quod volunt credunt",
+                               "Hostium munera, non munera" };
+
+    std::cout << "svec.size() = " << svec.size() << "\n";
+    std::cout << "svec.capacity() = " << svec.capacity() << "\n";
+
+//    svec.resize(svec.capacity() + 1);
+    svec.push_back("Factis ut credam facis");
+}
+
+// Yukaridaki durum necatiergin'in github sayfasindan alindi. Kod incelendiginde
+// su durum ortaya cikiyor. Eger moveCtor noExcept degilse, pushback fonksiyonu
+// strong garanti verdigi icin, copyCtor'u cagirir. Bu da fazla maliyet demek!
+// Eger moveCtor noexcept ise, movector'u otomatik olarak cagirarak optimizasyon
+// yapmis olur. Yani noexcept oyle yamana atilacak bir konu degildir!!!
+
+/////////////////////////////////////////////////////////////////////////////////////////
+// Yukarida hep noexcept specifier anlatildi. Simdi noexcept operator anlatilacak.
+
+class nai{
+public:
+    nai() = default;
+    void nai_000();
+    void nai_001()noexcept;
+};
+
+void main_14(){
+    nai Nai;
+
+    const auto b = noexcept(Nai.nai_000()); // incelenirse b'nin degerinin false oldugu gorulur.
+    const auto b1 = noexcept(Nai.nai_001()); // incelenirse b'nin degerinin true oldugu gorulur.
+    const auto b2 = noexcept(nai{}); // default Ctor'un noexcept garanti oldugu gorulur. Ama ctor'u biz yazsaydik,
+                                     // ve noexcept garanti belirtmeseydik, bu sefer false olacaktı.
 
 
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// unevaluated context --> sonucun degerlendirilmedigi ve etki etmedigi durumlari ifade eder.
+// sizeof, decltype ve noexcept operator, unevaluated context sınıfındadır.
+
+void main_15(){
+    int x = 15;
+
+    constexpr auto b = noexcept(x+20);
+    std::cout << x << "\n";
+
+    std::cout << sizeof(x++) <<"\n";
+    std::cout << x << "\n";
+
+    decltype(x++) a;
+    std::cout << x <<"\n";
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+class nai_001 {
+};
+
+void naif_000(const nai_001& r) noexcept(noexcept(nai_001{}));
+// Yukaridaki parantez içindeki noexcept, operator olur. Default ctor oldugundan, true gelir.
+// yani void naif_000(const nai_001& r) noexcept(true)); --> bununla aynıdır.
+
+///////////////////////////////////////////////////////////////////////////////////////////
 
 
+int foo(int); // --> Noexcept garantisi yok!!!
 
+void naif_001()
+{
+    int (*fp)(int)noexcept; // Bu fonksiyon, noexcept garantisi veren bir fonksiyonu gösterecek.
+//    fp = &foo; // Yorum satiri kaldirilirsa gorulecektir ki, foo fonksiyonu noexcept garantisi
+    // vermedigi icini, hata vardir.
+}
 
+///////////////////////////////////////////////////////////////////////////////////////////
 
+class nai_002 {
+public:
+    virtual void naif_002()noexcept;
+};
 
+class nai_003 : public nai_002 {
+public:
+//    void naif_002()override; // yorum satırı kaldırırlırsa hata gorulur.
+    // kalitim yoluyla alinan metotun  except garantisi çiğnenmiş oluyor çünkü!!!
+    void naif_002()noexcept override; // bu sekilde yapilirsa, hata olmadigi gorulur.
+};
 
+///////////////////////////////////////////////////////////////////////////////////////////
+// Destructor --> destructor nothrow garantisi vermek ZORUNDA.
+// Destructor ya hiç exception throw etmeyecek, edecekse de kendi içerisinde yakalayacak!!!
 
+// Constructor --> Exception gondermeye en fazla aday olan fonksiyon ne diye sorsaniz
+// cevap kesinlikle constructor'dır.
 
+///////////////////////////////////////////////////////////////////////////////////////////
 
+class nai_004 {
+public:
+    nai_004() {
+        throw 1;
+    }
 
+    ~nai_004() {
 
+    }
 
+    unsigned char nai_uc[1024]{};
+};
 
+void main_16()
+{
+    nai_004* nainai = new nai_004;
 
+    // burada constructor exception throw etse bile, unstack unwinding devreye girer ve,
+    // kaynaklar geri verilir. Bu açıdan bir sıkıntı yok.
+}
 
-
-
-
-
-
-
-
-
-
-
-
+///////////////////////////////////////////////////////////////////////////////////////////
 
 int main()
 {
-    main_10();
+    main_15();
     return 0;
 }
 
